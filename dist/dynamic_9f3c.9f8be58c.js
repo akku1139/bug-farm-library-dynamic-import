39539293e4f34f4024ac0ab7204d98fae5e0a1da
-akku- <akkun11.open@gmail.com>
2025-03-23 21:44:17 +0900

    add dist

---
+++ dist/dynamic_9f3c.9f8be58c.js
@@ -0,0 +1,6 @@
+var dynamic_default = "dynamic.js data";
+var dynamic_ns = {
+    "default": dynamic_default,
+    __esModule: true
+};
+export default dynamic_default;
---
+++ dist/entry.js
@@ -0,0 +1,49 @@
+function _interop_require_default(obj) {
+    return obj && obj.__esModule ? obj : {
+        default: obj
+    };
+}function _export_star(from, to) {
+    Object.keys(from).forEach(function(k) {
+        if (k !== "default" && !Object.prototype.hasOwnProperty.call(to, k)) {
+            Object.defineProperty(to, k, {
+                enumerable: true,
+                get: function() {
+                    return from[k];
+                }
+            });
+        }
+    });
+    return from;
+}function _interop_require_wildcard(obj, nodeInterop) {
+    if (!nodeInterop && obj && obj.__esModule) return obj;
+    if (obj === null || typeof obj !== "object" && typeof obj !== "function") return {
+        default: obj
+    };
+    var cache = _getRequireWildcardCache(nodeInterop);
+    if (cache && cache.has(obj)) return cache.get(obj);
+    var newObj = {
+        __proto__: null
+    };
+    var hasPropertyDescriptor = Object.defineProperty && Object.getOwnPropertyDescriptor;
+    for(var key in obj){
+        if (key !== "default" && Object.prototype.hasOwnProperty.call(obj, key)) {
+            var desc = hasPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : null;
+            if (desc && (desc.get || desc.set)) Object.defineProperty(newObj, key, desc);
+            else newObj[key] = obj[key];
+        }
+    }
+    newObj.default = obj;
+    if (cache) cache.set(obj, newObj);
+    return newObj;
+}function _getRequireWildcardCache(nodeInterop) {
+    if (typeof WeakMap !== "function") return null;
+    var cacheBabelInterop = new WeakMap();
+    var cacheNodeInterop = new WeakMap();
+    return (_getRequireWildcardCache = function(nodeInterop) {
+        return nodeInterop ? cacheNodeInterop : cacheBabelInterop;
+    })(nodeInterop);
+}var static_default = "static.js data";
+
+const dynamic = (await import("__FARM_BUNDLE_REFERENCE_SLOT__:dynamic_9f3c_9f3c4808f93360665f0ce703d086625c_js")).default;
+console.log("static import:", static_default);
+console.log("dynamic import:", dynamic);
